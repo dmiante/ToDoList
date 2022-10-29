4fa85e1ccc2a99e41ac9366503a03039d2ca053e
dmi4n <the.dmian@live.cl>
2022-10-29 01:45:20 -0300

    ADD update and delete functions

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToDoItem(props) {
+export default function ToDoItem({item, onUpdate, onDelete}) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [updateTask, setUpdateTask] = useState(item.title ?? '')
+
+  function handleChange(event) {
+    setUpdateTask(event.target.value);
+  }
+
+  function handleUpdate(){
+    onUpdate(item.id, updateTask)
+    setIsEdit(false)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    onUpdate(item.id, updateTask);
+    setIsEdit(false)
+  }
+
+
   return (
-    <div
-      onClick={() => {
-        props.onChecked(props.id);
-      }}
-    >
-      <li>{props.text}</li>
+    <div>
+      {isEdit ? (
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+          autoFocus
+          onChange={handleChange}
+          value={updateTask}
+          onBlur={(evt) => {
+            setIsEdit(false)
+            setUpdateTask(evt.target.value)
+            }} 
+            />
+            {/* onMouseDown have more priority than onblur */}
+          <button className="btnAdd" onMouseDown={handleUpdate}>
+            <span>Update</span>
+          </button>
+        </form>
+      ) : (
+        <div className="listItem">
+          <span
+            className="taskUpdate"
+            onDoubleClick={() => {
+              setIsEdit(true)
+            }}
+          >
+            {item.title}
+          </span>
+          <button
+            className="detelebtn"
+            onClick={() => 
+              onDelete(item.id)
+            }
+          >
+            Delete
+          </button>
+        </div>
+      )}
     </div>
   );
 }
-
-export default ToDoItem;
